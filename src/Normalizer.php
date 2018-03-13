bc9e5fae232b032cd82aae8e5c1384618899698f
Elan Ruusamäe <glen@delfi.ee>
2018-03-14 00:36:36 +0200

    normalize unicode, strip control chars

--- src/Normalizer.php
+++ src/Normalizer.php
@@ -1,17 +1,34 @@
 <?php
+
 namespace glen\FilenameNormalizer;
+
+use Normalizer as IntlNormalizer;
 
 class Normalizer
 {
     /**
-     * @param $name
+     * Make filename safe:
+     * - Unicode normalize NFC
+     * - Replace unsafe characters to be portable
+     *
+     * @see http://windows.microsoft.com/en-us/windows/file-names-extensions-faq File names and file name extensions: frequently asked questions (Windows)
+     * @see https://support.apple.com/en-us/HT202808 OS X: Cross-platform filename best practices and conventions (OSX)
+     * @param string $name
      * @param string $replacement
-     * @return mixed
+     * @return string
      */
-    public static function normalize($name, $replacement = '-')
+    public static function normalize($name, $replacement = "-")
     {
-        $entries = array('\\', '/', '?', ':', '*', '"', '>', '<', '|');
+        // Unicode NFC
+        // https://en.wikipedia.org/wiki/Unicode_equivalence#Normal_forms
+        $name = IntlNormalizer::normalize($name, IntlNormalizer::FORM_C);
 
-        return str_replace($entries, $replacement, $name);
+        // OS Safe characters
+        $name = str_replace(array('\\', '/', '?', ':', '*', '"', '>', '<', '|'), $replacement, $name);
+
+        // strip control chars, backspace and delete (including \r)
+        $name = preg_replace('/[\x00-\x08\x0b-\x1f\x7f]/', '', $name);
+
+        return $name;
     }
 }
